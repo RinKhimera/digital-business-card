4d6a11039bddf95f2741c167c02da24a03e74025
Samuel Pokam <dixiades@gmail.com>
2022-12-31 18:52:20 +0100

    Links added

--- src/components/Info.js
+++ src/components/Info.js
@@ -12,10 +12,67 @@ const Info = () => {
           href="https://rinkhimera.github.io/"
           target="_blank"
           rel="noreferrer"
+          role="button"
+          className="btn btn-outline-light"
         >
           My Personal Website
         </a>
-        <button className="btn btn-primary">Click Me</button>
+        <div className="d-grid gap-3 d-flex py-2 justify-content-center">
+          <a
+            href="https://rinkhimera.github.io/"
+            target="_blank"
+            rel="noreferrer"
+            role="button"
+            className="btn btn-outline-primary"
+          >
+            Source Code
+          </a>
+          <div className="btn-group" role="group">
+            <button
+              type="button"
+              className="btn btn-outline-info dropdown-toggle"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              My links
+            </button>
+            <ul className="dropdown-menu">
+              <li>
+                <a
+                  className="dropdown-item"
+                  href="https://github.com/RinKhimera"
+                  target="_blank"
+                  rel="noreferrer"
+                  role="button"
+                >
+                  GitHub
+                </a>
+              </li>
+              <li>
+                <a
+                  className="dropdown-item"
+                  href="https://www.linkedin.com/in/samuel-pokam/"
+                  target="_blank"
+                  rel="noreferrer"
+                  role="button"
+                >
+                  LinkedIn
+                </a>
+              </li>
+              <li>
+                <a
+                  className="dropdown-item"
+                  href="mailto:dixiades@gmail.com"
+                  target="_blank"
+                  rel="noreferrer"
+                  role="button"
+                >
+                  Email
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
